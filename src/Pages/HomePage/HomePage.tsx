83e42f5c33f83953ad234e9250a120e1685114f3
Athena Rubio <75556016+SpeedyDolphin@users.noreply.github.com>
2023-11-05 14:35:56 -0800

    init setup

---
+++ src/Pages/HomePage/HomePage.tsx
@@ -0,0 +1,10 @@
+function HomePage() {
+    return (
+      <div className="HomePage">
+       <p>Hello!</p>
+      </div>
+    );
+  }
+  
+  export default HomePage;
+  
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './Pages/App';
+import HomePage from './Pages/HomePage/HomePage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -9,9 +11,38 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+     <BrowserRouter>
+      <Routes>
+        <Route index element={<App/>}/>
+        <Route path="/Home"   element={<HomePage/>} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
+
+// root.render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <AuthProvider>
+//         <Routes>
+//           <Route path="/" exact errorElement={<PermissionDenied />}>
+//             <Route index element={<Login/>}/>
+//             <Route path="/superAdminHome"   element={<SuperAdminHome/>} />
+//             <Route path="/addManufacturer"  element={<AddManufacturer />} />
+//             <Route path="/addRetailer"      element={<AddRetailer/>} />
+//             <Route path="/editManufacturer/:manufacturer" element={<EditManufacturer />} />
+//             <Route path="/manufacturerHome" element={<ManufacturerHome />} />
+//             <Route path="/playground" element={<Playground />} />
+//             <Route path="/manufacturerHome/:manufacturerId" element={<ManufacturerHome />} />
+//             <Route path="/ViewRetailer" element={<ViewRetailer />} />
+//             <Route path='*' element={<PermissionDenied />} />
+//           </Route>
+//         </Routes>
+//       </AuthProvider>
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
