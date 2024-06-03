1b1b026aa4286a3a94efb6bc5bcb29338a797ada
Athena Rubio <75556016+SpeedyDolphin@users.noreply.github.com>
2024-06-02 19:01:37 -0700

    figured out deployment problem and set temporary background image

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -5,7 +5,6 @@ function HomePage() {
   return (
     <div className="HomePage">
       <p className={styles.header}>Hello!</p>
-      <img className={styles.landingImg} src='/images/versaillesGarden.jpg' alt='Girl sitting in Versailles Garden'/>
       <Link to="/temp"> Link Test </Link>
     </div>
     );
